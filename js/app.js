c0f7068d9af319a3ee541a59d600f9cb08519612
ppotaczek <ppotaczek8@gmail.com>
2017-06-07 19:47:49 +0200

    Basic structure, adding new profiles

---
+++ js/app.js
@@ -0,0 +1,45 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", function(){
+  var system = {
+    table : document.getElementById('table'),
+    profilesList : [],
+
+    addProfile : function(){
+      var name = document.getElementById('name').value;
+      var carrots = document.getElementById('carrots').value;
+      var profile = new Profile(name, carrots);
+      this.profilesList.push(profile);
+      this.addToTable();
+    },
+
+    addToTable : function(){
+      this.cleanTable();
+      for (var i = 0; i < this.profilesList.length; i++){
+        var row = this.table.insertRow();
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        var cell3 = row.insertCell(2);
+        cell1.innerText = i+1+".";
+        cell2.innerText = this.profilesList[i].name;
+        cell3.innerText = this.profilesList[i].carrots;
+      }
+    },
+
+    cleanTable : function(){
+      while(this.table.rows.length > 1) {
+        table.deleteRow(1);
+      }
+    }
+  }
+
+  function Profile(name, carrots) {
+    this.name = name;
+    this.carrots = carrots;
+  }
+
+  function main(){
+    system.addProfile();
+  }
+  document.getElementById('add').addEventListener("click", main);
+});
