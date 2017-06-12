3df12717cb6998e0d0d8cac071701c5341601248
ppotaczek <ppotaczek8@gmail.com>
2017-06-12 12:49:23 +0200

    Almost all functionality

--- js/app.js
+++ js/app.js
@@ -4,16 +4,36 @@ document.addEventListener("DOMContentLoaded", function(){
   var system = {
     table : document.getElementById('table'),
     profilesList : [],
+    operationsList : ["- 50", "- 20", "- 10", "- 5", "- 2", "- 1", "+ 1", "+ 2", "+ 5", "+ 10", "+ 20", "+ 50"],
+    tip : document.getElementById('tip'),
 
-    addProfile : function(){
-      var name = document.getElementById('name').value;
-      var carrots = document.getElementById('carrots').value;
-      var profile = new Profile(name, carrots);
+    addProfile : function(){      // Add and check new profile
+      var name = document.getElementById('name');
+      var carrots = document.getElementById('carrots');
+
+      for (var i = 0; i < this.profilesList.length; i++){
+        if (name.value == this.profilesList[i].name){
+          this.tip.innerText = "Profil o takiej nazwie już istnieje!";
+          return false;
+        }
+      }
+      var profile = new Profile(name.value, carrots.value);
+      this.tip.innerText = "";
       this.profilesList.push(profile);
-      this.addToTable();
+      name.value = "";
+      carrots.value = "";
+      this.drawTable();
     },
 
-    addToTable : function(){
+    sortArray : function(){
+      this.profilesList.sort(function(a, b){
+        return b.carrots - a.carrots
+      });
+    },
+
+    drawTable : function(){     // Fill table
+      this.sortArray();
+      data.save();
       this.cleanTable();
       for (var i = 0; i < this.profilesList.length; i++){
         var row = this.table.insertRow();
@@ -23,16 +43,62 @@ document.addEventListener("DOMContentLoaded", function(){
         cell1.innerText = i+1+".";
         cell2.innerText = this.profilesList[i].name;
         cell3.innerText = this.profilesList[i].carrots;
+        this.addOperations(row);
+      }
+      this.addOperationsFunc();
+      this.addRemoveFunc();
+    },
+
+    addOperations : function(line){     // Add buttons to row
+      var cell4 = line.insertCell(3);
+      var cell5 = line.insertCell(4);
+      var newBtn;
+
+      for (var i = 0; i < this.operationsList.length; i++){
+        newBtn = document.createElement("button");
+        newBtn.classList.add("operation");
+        newBtn.innerText = this.operationsList[i];
+        cell4.appendChild(newBtn);
+      }
+      cell5.innerHTML = "<button class='remove'>X</button>";
+    },
+
+    addOperationsFunc : function(){     // Add mathematical function to buttons
+      var operationBtns = document.getElementsByClassName("operation");
+
+      for (var i = 0; i < operationBtns.length; i++){
+        operationBtns[i].addEventListener("click", function(){
+          var order = this.parentElement.parentElement.rowIndex;
+          var actualVal = system.profilesList[order-1].carrots;
+          var btnVal = (this.innerText).replace(' ','');
+
+          actualVal = parseInt(actualVal) + parseInt(btnVal);
+          actualVal < 0 ? actualVal = 0: true;
+          system.profilesList[order-1].carrots = actualVal;
+          system.drawTable();
+        });
       }
     },
 
-    cleanTable : function(){
+    addRemoveFunc : function(){     // Add remove function to buttons
+      var removeBtns = document.getElementsByClassName("remove");
+      for (var i = 0; i < removeBtns.length; i++){
+        removeBtns[i].addEventListener("click", function(){
+          var order = this.parentElement.parentElement.rowIndex;
+          table.deleteRow(order);
+          system.profilesList.splice(order-1, 1);
+          system.drawTable();
+        });
+      }
+    },
+
+    cleanTable : function(){      // Remove all rows from table
       while(this.table.rows.length > 1) {
         table.deleteRow(1);
       }
     }
   }
-  
+
   var data = {      // Save and get data
     save : function(){
       localStorage.setItem('profiles', JSON.stringify(system.profilesList));
@@ -47,13 +113,20 @@ document.addEventListener("DOMContentLoaded", function(){
     }
   }
 
-  function Profile(name, carrots) {
+  function Profile(name, carrots) {     // Constructor to create new profile object
     this.name = name;
     this.carrots = carrots;
   }
 
-  function main(){
+  function add(){
     system.addProfile();
   }
-  document.getElementById('add').addEventListener("click", main);
+
+  function main(){
+    document.getElementById('add').addEventListener("click", add);
+    data.getData();
+    system.drawTable();
+  }
+
+  main();
 });
